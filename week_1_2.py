ba2f4b4afd583c90fbda72bb6a296c03e78cc369
Nicolai <nicolaibthomsen@hotmail.dk>
2020-08-26 21:27:27 +0200

    improved efficiency with np.arrays

--- week_1_2.py
+++ week_1_2.py
@@ -1,18 +1,17 @@
 import sys
-import numpy as np
+from numpy import dot, array
 
 
 n, k = sys.stdin.readline().strip("\n").split()
-v = [[int(i) for i in sys.stdin.readline().strip("\n").split()] for _ in range(int(k))]
+v = array([array([int(i) for i in sys.stdin.readline().strip("\n").split()]) for _ in range(int(k))])
 blank = sys.stdin.readline() # middle line
-w = [[int(i) for i in sys.stdin.readline().strip("\n").split()] for _ in range(int(k))]
+w = array([array([int(i) for i in sys.stdin.readline().strip("\n").split()]) for _ in range(int(k))])
 
-results = 0
 
 def inner(n, v, w):
     for v_i in v:
         for w_i in w:
-            if np.dot(v_i, w_i) == 0:
+            if dot(v_i, w_i) == 0:
                 return "yes"
 
     return "no"
